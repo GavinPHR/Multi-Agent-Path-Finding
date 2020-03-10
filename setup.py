5051cf2b13919a8835b454f261846f134355e7f2
Gavin Peng <gavinsweden@gmail.com>
2020-03-10 15:00:17 +0000

    new start

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'multi_agent_path_finding',
+    packages = ['mapf-cbs'],
+    version = '0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Haoran Peng',
+    author_email = 'gavinsweden@gmail.com',
+    url = 'https://github.com/GavinPHR/Multi-Agent-Path-Finding',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
+# More mod to be done
